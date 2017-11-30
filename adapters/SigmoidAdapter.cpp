ff24aa918fa5db82b3312e7918ba7d1c1923a925
Philipp Weidel <p.weidel@fz-juelich.de>
2017-11-30 21:11:07 +0900

    added parameter for SigmoidAdapter

--- adapters/SigmoidAdapter.cpp
+++ adapters/SigmoidAdapter.cpp
@@ -20,9 +20,19 @@ SigmoidAdapter::SigmoidAdapter()
 
 void SigmoidAdapter::init(int argc, char** argv)
 {
+
+    scale = DEFAULT_SCALE;
+    slope = DEFAULT_SLOPE;
+    x_offset = DEFAULT_X_OFFSET;
+    y_offset = DEFAULT_Y_OFFSET;
+
     Adapter::init(argc, argv, "Sigmoid");
 
     // config needed for this specific adapter
+    setup->config("scale", &scale);
+    setup->config("slope", &slope);
+    setup->config("x_offset", &x_offset);
+    setup->config("y_offset", &y_offset);
 }
 
 void
@@ -30,7 +40,7 @@ SigmoidAdapter::tick()
 {
     for (int i = 0; i < port_in->data_size; ++i)
     {
-        port_out->data[i] = 1. / ( 1. + std::exp(- port_in->data[i]));
+        port_out->data[i] = scale / ( 1. + std::exp(- port_in->data[i] * slope + x_offset)) + y_offset;
     }
 }
 
--- adapters/SigmoidAdapter.h
+++ adapters/SigmoidAdapter.h
@@ -16,6 +16,11 @@
 #include "sys/time.h"
 #include "float.h"
 
+const double DEFAULT_SCALE = 1.0;
+const double DEFAULT_SLOPE = 1.0;
+const double DEFAULT_X_OFFSET = 0.0;
+const double DEFAULT_Y_OFFSET = 0.0;
+
 class SigmoidAdapter : public Adapter
 {
     public:
@@ -24,6 +29,7 @@ class SigmoidAdapter : public Adapter
         void tick();
 
     private:
+        double scale, slope, x_offset, y_offset;
 };
 
 #endif // SIGMOID_ADAPTER_H
